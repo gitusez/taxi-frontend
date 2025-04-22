3c5db954be5f5656ca8a174b94c1c30e506e29e0
gitusez <rob1zshirenyan@gmail.com>
2025-04-22 09:00:13 +0300

    back one loader

--- js/app.js
+++ js/app.js
@@ -72,14 +72,14 @@ document.addEventListener('DOMContentLoaded', () => {
       try {
         errorBox.style.display = "none";
         // if (firstLoad) loader.style.display = "block";
-        // loader.style.display = "block";
+        loader.style.display = "block";
         //dobavil
-        if (firstLoad) {
-          loader.style.display = "block";
-        } else {
-          inlineLoader.style.display = "block";
-        }
-        loadMoreBtn.style.display = "none";
+        // if (firstLoad) {
+        //   loader.style.display = "block";
+        // } else {
+        //   inlineLoader.style.display = "block";
+        // }
+        // loadMoreBtn.style.display = "none";
 
         const response = await fetch(config.apiUrl, {
           method: "POST",
@@ -116,7 +116,7 @@ document.addEventListener('DOMContentLoaded', () => {
       } finally {
         //dobavil
         loader.style.display = "none";
-        inlineLoader.style.display = "none";
+        // inlineLoader.style.display = "none";
         if (!allLoaded) loadMoreBtn.style.display = "block";
         firstLoad = false;
         
